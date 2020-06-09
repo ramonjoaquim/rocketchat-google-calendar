e23bc22ee9cf28e03e56d9d73255404732ae5d67
Ramon Joaquim <39497210+ramonjoaquim@users.noreply.github.com>
2020-06-09 17:39:37 -0300

    Update webhook.js
    tradução para pt-br

--- webhook.js
+++ webhook.js
@@ -10,7 +10,122 @@
 
 //function blatantly stolen
 function dateDiff(datepart, fromdate, todate) {	
+  datepart = datepart.toLowerCase();	/* exported Script */
+/* globals console, _, s */
+
+/** Global Helpers
+ *
+ * console - A normal console instance
+ * _       - An underscore instance
+ * s       - An underscore string instance
+ */
+
+//function blatantly stolen
+function dateDiff(datepart, fromdate, todate) {	
   datepart = datepart.toLowerCase();	
+  var diff = todate - fromdate;	
+  var divideBy = { w:604800000, 
+                   d:86400000, 
+                   h:3600000, 
+                   n:60000, 
+                   s:1000 };	
+  
+  return Math.floor( diff/divideBy[datepart]);
+}
+
+//function blatantly stolen and modified
+function getFormattedDate(d, offset){
+    // convert to msec
+    // add local time zone offset
+    // get UTC time in msec
+    var utc = d.getTime() + (d.getTimezoneOffset() * 60000);
+
+    // create new Date object for different city
+    // using supplied offset
+    var nd = new Date(utc + (3600000*offset));
+
+
+	dstr = d.getFullYear() + "-" + ('0' + (d.getMonth() + 1)).slice(-2) + "-" + ('0' + d.getDate()).slice(-2) + " " + nd.toLocaleTimeString();
+	//d = d.toLocaleString();  
+    return dstr;
+}
+
+
+
+
+
+class Script {
+  /**
+   * @params {object} request
+   */
+  process_incoming_request({ request }) {
+
+
+  //console is a global helper to improve debug
+	// console.log(request.content);
+    
+  var myContent = request.content; 
+     
+  if (myContent.start.dateTime) {
+		var dateStart  	= new Date(myContent.start.dateTime);
+	} else {
+		var dateStart  	= new Date(myContent.start.date);	
+	}
+
+	var dateStartText 	= getFormattedDate(dateStart,2);
+	
+  if (myContent.end.dateTime) {
+		var dateEnd  	= new Date(myContent.end.dateTime);
+	} else {
+		var dateEnd  	= new Date(myContent.end.date);	
+	}
+
+	var dateEndtText 	= getFormattedDate(dateEnd,2);
+
+	var duration = 	dateDiff('n', dateStart, dateEnd);
+	var durationText;
+
+	if (duration < 300) {
+		durationText = duration + ' minutos ';
+	} else {
+		duration = 	dateDiff('h', dateStart, dateEnd);
+		durationText = duration + ' horas ';	
+
+		if (duration > 24) {
+			duration = 	dateDiff('d', dateStart, dateEnd);
+			durationText = durationText + ' ( ' + duration + ' dias )';	
+		}	
+	}
+
+            
+    var myText = myContent.organizer.displayName ?? "" + " Google Calendar Event \n";
+    myText = myText + 'Resumo:  *' + myContent.summary + '*\n';
+
+    if (myContent.description) {
+      myText = myText + 'Descrição:      *' + myContent.description + '*\n';
+    }
+
+    if (myContent.location) {
+      myText = myText + 'Local:      *' + myContent.location + '*\n';     
+    }
+
+    myText = myText + 'Início:    *' + dateStartText +'*\n';    	
+	  myText = myText + 'Fim:    *' + dateEndtText +'*\n';     
+    myText = myText + 'Duração:      *' + durationText + ' * \n';
+    myText = myText + 'Link:	 ' + myContent.htmlLink + '\n';
+
+    if (myContent.hangoutLink) {
+    	myText = myText + 'Hangout:      ' + myContent.hangoutLink + '\n';    	
+    }
+    
+    return {
+      content:{
+        text: myText
+       }
+    };
+
+  }
+}
   var diff = todate - fromdate;	
   var divideBy = { w:604800000, 
                    d:86400000, 
